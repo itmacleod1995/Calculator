6a12d11da4a1515ad66f24f016440d3bf6b78144
Ian <imacleod@mail.umw.edu>
2022-06-11 10:14:17 -0400

    Add actionlisteners to all number buttons

--- javascript.js
+++ javascript.js
@@ -1,7 +1,4 @@
 const number = document.querySelector("span");
-console.log(number);
-
-const one = document.getElementById("one");
 
 one.addEventListener("click", function (e) {
     if (number.textContent == "0") {
@@ -20,5 +17,67 @@ two.addEventListener("click", function (e) {
         let newNumber = number.textContent + "" + 2;
         number.textContent = newNumber;
     }
+});
 
+three.addEventListener("click", function (e) {
+    if (number.textContent == "0") {
+        number.textContent = "3";
+    } else {
+        let newNumber = number.textContent + "" + 3;
+        number.textContent = newNumber;
+    }
+});
+
+four.addEventListener("click", function (e) {
+    if (number.textContent == "0") {
+        number.textContent = "4";
+    } else {
+        let newNumber = number.textContent + "" + 4;
+        number.textContent = newNumber;
+    }
+});
+
+five.addEventListener("click", function (e) {
+    if (number.textContent == "0") {
+        number.textContent = "5";
+    } else {
+        let newNumber = number.textContent + "" + 5;
+        number.textContent = newNumber;
+    }
+});
+
+six.addEventListener("click", function (e) {
+    if (number.textContent == "0") {
+        number.textContent = "6";
+    } else {
+        let newNumber = number.textContent + "" + 6;
+        number.textContent = newNumber;
+    }
+});
+
+seven.addEventListener("click", function (e) {
+    if (number.textContent == "0") {
+        number.textContent = "7";
+    } else {
+        let newNumber = number.textContent + "" + 7;
+        number.textContent = newNumber;
+    }
+});
+
+eight.addEventListener("click", function (e) {
+    if (number.textContent == "0") {
+        number.textContent = "8";
+    } else {
+        let newNumber = number.textContent + "" + 8;
+        number.textContent = newNumber;
+    }
+});
+
+nine.addEventListener("click", function (e) {
+    if (number.textContent == "0") {
+        number.textContent = "9";
+    } else {
+        let newNumber = number.textContent + "" + 9;
+        number.textContent = newNumber;
+    }
 });
